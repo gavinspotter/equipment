33aac6a0a5164794a195c5a99329808d51de0745
Osiris <potterosiris@gmail.com>
2021-01-16 20:14:14 -0500

    mw

--- server/app.js
+++ server/app.js
@@ -5,3 +5,17 @@ const mongoose = require("mongoose")
 const app = express()
 
 app.use(bodyParser.json())
+
+
+app.use((req, res, next) => {
+    const error = new HttpError("could not find this route", 404);
+    throw error;
+});
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || "an unknown error occured" });
+});
