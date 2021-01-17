a8d12fb0d1e22b837c6eaccfb42793d7c5a930fb
Osiris <potterosiris@gmail.com>
2021-01-16 20:30:09 -0500

    user schema

--- server/models/user.js
+++ server/models/user.js
@@ -3,3 +3,11 @@ const mongoose = require("mongoose")
 const uniqueValidator = require("mongoose-unique-validator")
 
 const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    equipment: [{ type: mongoose.Types.ObjectId, required: true }],
+    employees: [{ type: mongoose.Types.ObjectId, required: true }],
+})
