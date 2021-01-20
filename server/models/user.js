8b98d3877a6efaf5b5615542e25d79aa7289f200
Osiris <potterosiris@gmail.com>
2021-01-19 21:54:56 -0500

    s udate

--- server/models/user.js
+++ server/models/user.js
@@ -8,7 +8,6 @@ const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
-    equipment: [{ type: mongoose.Types.ObjectId, required: true }],
     company: [{ type: mongoose.Types.ObjectId, required: true }]
 })
 
