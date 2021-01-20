c87953e54083674956aa82317a49a538a47acbfa
Osiris <potterosiris@gmail.com>
2021-01-19 21:56:54 -0500

    ref

--- server/models/user.js
+++ server/models/user.js
@@ -8,7 +8,7 @@ const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
-    company: [{ type: mongoose.Types.ObjectId, required: true }]
+    company: [{ type: mongoose.Types.ObjectId, required: true, ref: "Company" }]
 })
 
 userSchema.plugin(uniqueValidator)
