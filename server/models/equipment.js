fbfa3e91e5b57a89c9865d656037231613297cc0
Osiris <potterosiris@gmail.com>
2021-01-19 21:48:27 -0500

    ref

--- server/models/equipment.js
+++ server/models/equipment.js
@@ -5,9 +5,8 @@ const Schema = mongoose.Schema
 const equipmentSchema = new Schema({
     name: { type: String, required: true },
     company: { type: mongoose.Types.ObjectId, required: true },
-    users: [{ type: mongoose.Types.ObjectId, required: true }],
     ehistory: [{ type: Date, required: true },
-    [{ type: mongoose.Types.ObjectId, required: true }]
+    [{ type: mongoose.Types.ObjectId, required: true, ref: "User" }]
     ]
 })
 
