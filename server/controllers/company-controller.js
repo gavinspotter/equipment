025459e944f40e6ea60e6f7a9a80ea464b445eb7
Osiris <potterosiris@gmail.com>
2021-04-03 14:02:17 -0400

    let hashed pw

--- server/controllers/company-controller.js
+++ server/controllers/company-controller.js
@@ -27,6 +27,15 @@ const signup = async (req, res, next) => {
 
     let hashedPassword;
 
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    } catch (err) {
+        const error = new HttpError(
+            'Could not create user, please try again.',
+            500
+        );
+        return next(error);
+    }
 
 }
 
