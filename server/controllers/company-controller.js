de03f59ec11f4e8f04c99b739239c71867714358
Osiris <potterosiris@gmail.com>
2021-04-03 13:43:01 -0400

    eh oid

--- server/controllers/company-controller.js
+++ server/controllers/company-controller.js
@@ -1,3 +1,4 @@
 const HttpError = require("../models/HttpError")
 
 const Company = require("../models/company")
+
--- server/models/equipment.js
+++ server/models/equipment.js
@@ -2,10 +2,19 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
 
+const equipmentHistorySchema = new Schema({
+    dateOfUse: { type: String, required: true },
+    users: [{ type: mongoose.Types.ObjectId, required: true, ref: "User" }],
+    jobDescription: { type: String, required: true }
+})
+
 const equipmentSchema = new Schema({
     name: { type: String, required: true },
     company: { type: mongoose.Types.ObjectId, required: true, ref: "Company" },
+    eHistory: [equipmentHistorySchema]
 
 })
+
+const equipmentHistoryOID = mongoose.model("Ehistory", equipmentHistoryOID)
 
 module.exports = mongoose.model("Equipment", equipmentSchema)
