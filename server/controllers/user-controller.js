4a12ba3ef1d2da1e720f6dcb0e59b7ee2ace99aa
Osiris <potterosiris@gmail.com>
2021-04-06 13:58:55 -0400

    hashed pw

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -40,7 +40,7 @@ const signup = async (req, res, next) => {
     const createdUser = new User({
         name,
         email,
-        password,
+        password: hashedPassword,
         company: [],
         equipment: []
     })
