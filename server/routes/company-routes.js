76fb2db6738f6cc5d6867eb614fad6e6c7a1f18f
Osiris <potterosiris@gmail.com>
2021-04-06 13:37:30 -0400

    check auth const

--- server/routes/company-routes.js
+++ server/routes/company-routes.js
@@ -5,6 +5,9 @@ const companyController = require("../controllers/company-controller")
 
 const router = express.Router()
 
+
+const checkAuth = require('../middleware/check-auth');
+
 router.post("/signup", companyController.signup)
 
 router.post("/login", companyController.login)
