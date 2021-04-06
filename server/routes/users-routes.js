c33f4e8d2e68169e106a6003126363a67892a821
Osiris <potterosiris@gmail.com>
2021-04-06 16:16:57 -0400

    check auth

--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -2,11 +2,18 @@ const express = require("express")
 
 const usersController = require("../controllers/user-controller")
 
+
 const router = express.Router()
+
+
+const checkAuth = require('../middleware/check-auth');
 
 router.post("/signup", usersController.signup)
 
 router.post("/login", usersController.login)
+
+
+router.use(checkAuth);
 
 router.post("/takeEquipment", usersController.takeEquipment)
 
