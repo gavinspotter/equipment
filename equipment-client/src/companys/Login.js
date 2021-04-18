3929b7e263400958b36ebf6c9eab0266b5de235e
Osiris <potterosiris@gmail.com>
2021-04-18 18:19:00 -0400

    jsx

--- equipment-client/src/companys/Login.js
+++ equipment-client/src/companys/Login.js
@@ -11,6 +11,8 @@ import { AuthContext } from "../../shared/context/auth-context"
 import { useHttpClient } from "../../shared/hooks/http-hook"
 import ErrorModal from '../shared/components/UIElements/ErrorModal'
 import LoadingSpinner from '../shared/components/UIElements/LoadingSpinner'
+import Input from "../shared/components/FormElements/Input"
+import Button from '../shared/components/FormElements/Button'
 
 
 
@@ -30,7 +32,21 @@ const Login = () => {
                 {isLoading && <LoadingSpinner asOverlay />}
                 <h2> Company Login </h2>
                 <form>
-
+                    <Input
+                        name="username"
+                        valRef={register}
+                        label="username"
+                        element="input"
+                    />
+                    <Input
+                        name="password"
+                        valRef={register}
+                        label="password"
+                        element="input"
+                        type="password"
+                    />
+                    did you mean to signup
+                    <Button> login </Button>
                 </form>
             </div>
         </React.Fragment>
