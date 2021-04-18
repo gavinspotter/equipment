6d89da7b99c76b8b71db6a73896344458cfba738
Osiris <potterosiris@gmail.com>
2021-04-18 18:09:30 -0400

    company login so far

--- equipment-client/src/companys/Login.js
+++ equipment-client/src/companys/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 
 import { useForm } from 'react-hook-form'
@@ -9,14 +9,31 @@ import { Link } from "react-router-dom"
 
 import { AuthContext } from "../../shared/context/auth-context"
 import { useHttpClient } from "../../shared/hooks/http-hook"
+import ErrorModal from '../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../shared/components/UIElements/LoadingSpinner'
 
 
 
 const Login = () => {
-    return (
-        <div>
 
-        </div>
+
+    const auth = useContext(AuthContext)
+
+    const { isLoading, error, sendRequest, clearError } = useHttpClient()
+
+    const { register, handleSubmit } = useForm()
+
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            <div>
+                {isLoading && <LoadingSpinner asOverlay />}
+                <h2> Company Login </h2>
+                <form>
+
+                </form>
+            </div>
+        </React.Fragment>
     )
 }
 
