245edd64272bd8cfe46e579ff441b01d92f18cec
Osiris <potterosiris@gmail.com>
2021-04-22 15:30:29 -0400

    sass

--- equipment-client/src/shared/HomePage/HomePage.js
+++ equipment-client/src/shared/HomePage/HomePage.js
@@ -11,7 +11,7 @@ const HomePage = () => {
     return (
         <div className="home">
             <CompanyLogin
-                companyLoginCard=""
+                companyLoginCard="card__company--login"
             />
             <CompanySignup
                 companySignupCard=""
