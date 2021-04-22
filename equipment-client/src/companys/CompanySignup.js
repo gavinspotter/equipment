4c8156c16413b59ff6467486fc465f3641588b0d
Osiris <potterosiris@gmail.com>
2021-04-22 14:22:11 -0400

    signup imports

--- equipment-client/src/companys/CompanySignup.js
+++ equipment-client/src/companys/CompanySignup.js
@@ -1,4 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+
+import { useForm } from 'react-hook-form'
+
+
+import { Link } from "react-router-dom"
+
+
+import { AuthContext } from "../shared/context/auth-context"
+import { useHttpClient } from "../shared/hooks/http-hook"
+import ErrorModal from '../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../shared/components/UIElements/LoadingSpinner'
+import Input from "../shared/components/FormElements/Input"
+import Button from '../shared/components/FormElements/Button'
+
 
 const CompanySignup = () => {
     return (
