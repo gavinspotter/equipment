efd98d9304f6229923f960065e9b9598318e7182
Osiris <potterosiris@gmail.com>
2021-04-22 14:26:20 -0400

    company sign up form

--- equipment-client/src/companys/CompanySignup.js
+++ equipment-client/src/companys/CompanySignup.js
@@ -16,9 +16,55 @@ import Button from '../shared/components/FormElements/Button'
 
 
 const CompanySignup = () => {
+
+
+    const auth = useContext(AuthContext)
+
+    const { isLoading, error, sendRequest, clearError } = useHttpClient()
+
+    const { register, handleSubmit } = useForm()
+
+
+    const onSubmit = async (data) => {
+        console.log(data)
+        try {
+            const responseData = await sendRequest(
+                `http://localhost:5000/api/company/signup`,
+                "POST",
+                JSON.stringify({
+                    username: data.username,
+                    password: data.password
+                }),
+                {
+                    "Content-Type": "application/json"
+                }
+            )
+            auth.companyLogin(responseData.companyId, responseData.companyToken)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <div>
+            <ErrorModal error={error} onClear={clearError} />
+            <div>
+                {isLoading && <LoadingSpinner asOverlay />}
+                <h2> Company Login </h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                        {...register("username")}
 
+                    />
+                    <br />
+                    <input
+                        {...register("password")}
+                        type="password"
+                    />
+                    did you mean to login
+                    <Button> signup </Button>
+                </form>
+            </div>
         </div>
     )
 }
