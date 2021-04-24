d15201cd425f772dbee4d703f4a2a4028d34f6b2
Osiris <potterosiris@gmail.com>
2021-04-24 12:16:41 -0400

    employee login

--- equipment-client/src/employees/EmployeeLogin.js
+++ equipment-client/src/employees/EmployeeLogin.js
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react'
 
 import { useForm } from "react-hook-form"
+import Button from '../shared/components/FormElements/Button'
+import ErrorModal from '../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../shared/components/UIElements/LoadingSpinner'
 
 import { AuthContext } from "../shared/context/auth-context"
 import { useHttpClient } from "../shared/hooks/http-hook"
 
 
-const Login = () => {
+const Login = (props) => {
 
     const auth = useContext(AuthContext)
 
@@ -14,10 +17,48 @@ const Login = () => {
 
     const { register, handleSubmit } = useForm()
 
+    const onSubmit = async (data) => {
+
+        try {
+            const responseData = await sendRequest(
+                'http://localhost:5000/api/user/login',
+                "POST",
+                JSON.stringify({
+                    email: data.email,
+                    password: data.password
+                }),
+                {
+                    "Content-Type": "application/json"
+                }
+            )
+            auth.login(responseData.userId, responseData.token)
+        } catch (err) {
+
+        }
+    }
 
     return (
-        <div>
+        <div className="">
+            <div className="">
 
+                <ErrorModal error={error} onClear={clearError} />
+                <div>
+                    {isLoading && <LoadingSpinner asOverlay />}
+                    <h2> Company Login </h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input
+                            {...register("email")}
+
+                        />
+                        <br />
+                        <input
+                            {...register("password")}
+                            type="password"
+                        /><br />
+                        <Button> login </Button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
