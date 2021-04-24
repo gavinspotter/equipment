9769f45aad677986bb3f72341c744135bdeaf882
Osiris <potterosiris@gmail.com>
2021-04-24 12:18:14 -0400

    employee sign in

--- equipment-client/src/employees/EmployeeSignup.js
+++ equipment-client/src/employees/EmployeeSignup.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { useForm } from "react-hook-form"
+import Button from '../shared/components/FormElements/Button'
+import ErrorModal from '../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../shared/components/UIElements/LoadingSpinner'
+
+import { AuthContext } from "../shared/context/auth-context"
+import { useHttpClient } from "../shared/hooks/http-hook"
 
 const Signup = () => {
     return (
