cf449ea7e991fa9056fcbdd9c99851ef84ee4d7a
Osiris <potterosiris@gmail.com>
2021-05-01 15:45:50 -0400

    use context

---
+++ equipment-client/src/employees/pages/EmployeeHome.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+
+import { AuthContext } from "../../shared/context/auth-context"
+
+const EmployeeHome = () => {
+
+
+    const auth = useContext(AuthContext)
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default EmployeeHome
